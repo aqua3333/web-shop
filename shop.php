23bc7cdde9f6764abb5344038a55fcb20fdc3981
aqua3333 <aqua.zms@gmail.com>
2016-03-15 10:24:48 +0200

    Added files via upload

---
+++ shop.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+ <title>My First Website</title>
+ <meta charset="utf-8" />
+ <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+ <link href="css/css.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<div id="block-body">
+
+<?php
+	include("include/block-header.php");	
+?>
+<div id="block-right"></div>
+<div id="block-content"></div>
+<?php
+	include("include/block-footer.php");
+?>
+</div>
+</body>
+</html>
